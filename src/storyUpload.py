f61bfa741edaa96be12f72e42aae8b11e453384c
Tyler Hagen <maverick917@msn.com>
2017-02-17 14:23:02 -0600

    moment upload working - initial functionality complete

--- src/storyUpload.py
+++ src/storyUpload.py
@@ -3,10 +3,32 @@ import csv
 import requests 
 import json
 
-#global variables
+#global variables - turn these into params api entry 
 storyuploadurl = 'http://localhost:4060/api/stories'
 momentuploadurl = 'http://localhost:4060/api/moments'	
 headers = {'content-type': 'application/json', 'Authorization': 'Token token="exploreapollo"'}
+
+
+
+def upload_moment(momentTitle, momentDescription, met_start, met_end, channel_id, story_id):
+	'''
+		uploads a moment with momentTitle, momentDescription, met_start (int),
+		met_end(int), channel_id (int), story_id(int)
+		
+		Need to add error handling (can tell by catching ValueError on response.json()) 
+	'''
+	momentparams = {'title' : '', 'description' : '', 'met_start': 0, 'met_end' : 0, 'channel_ids' : [], 'story_ids' : []}
+
+	momentparams['title'] = momentTitle
+	momentparams['description'] = momentDescription
+	momentparams['met_start'] = met_start
+	momentparams['met_end'] = met_end
+	momentparams['channel_ids'].append(channel_id)
+	momentparams['story_ids'].append(story_id)
+	
+	response = requests.post(momentuploadurl, json=momentparams, headers=headers)
+	
+	
 
 def upload_story(storyTitle, storyDescription):
 	'''
@@ -54,9 +76,10 @@ with open('{0}.csv'.format(storyTitle), 'rb') as csvfile:
 		else:
 			useTitle = '{0}_{1}'.format(lastTitle, i)
 			i = i + 1
-
-		print useTitle, moment['met_start'], moment['met_end'], moment['Transcript Files']
 		
+		channelID = int(moment['Transcript Files'].split('_')[2][2:]) #yikes
+		print useTitle, moment['met_start'], moment['met_end'], channelID
+		upload_moment(useTitle, "placeholders description", moment['met_start'], moment['met_end'], channelID, storyId)
 
 		
 
