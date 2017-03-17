970bc27274e0f67f1d4c48674bb36e08ad517759
Satsuki Ueno <satsukiueno@gmail.com>
2017-03-17 09:49:53 -0500

    Manually corrected a11 photo timestamps

--- src/ExtractMET.py
+++ src/ExtractMET.py
@@ -3,7 +3,10 @@ This script extracts met times for images from
 https://www.hq.nasa.gov/alsj/a11/images11.html.
 
 This script should not be expected to work
-for any other page. 
+for any other page, and the output should 
+be manually corrected as there are natural
+language annotations indicating time beyond
+the ability of this simple script.
 '''
 
 from html.parser import HTMLParser
@@ -12,25 +15,35 @@ import sys
 import re
 
 def toMet(missionTime):
-	hours = int(missionTime[0:3])
-	minutes = int(missionTime[4:6])
-	seconds = int(missionTime[7:9])
+	timeSlots = missionTime.split(':')
+	
+	hours = int(timeSlots[0])
+	minutes = int(timeSlots[1])
+	if len(timeSlots) == 3:
+		seconds = int(timeSlots[2])
+	else:
+		seconds = 0
 	
 	totsecs = (hours * 60 + minutes) * 60 + seconds
 	return totsecs * 1000
 
 
 class MetExtractor(HTMLParser):
+	EMPTY_ELEMS = set(['area','base','br','col','embed','hr','img',
+		'input','link','meta','param','source','track','wbr'])
 	
 	def __init__(self):
 		HTMLParser.__init__(self)
 		self._activeRegion = False
 		self._tagStack = LifoQueue()
 		self.idRegex = re.compile('AS\d{2}-\d{2}-\d{4}')
-		self.timeRegex = re.compile('\d{3}:\d{2}:\d{2}')
+		self.timeRegex = re.compile('\d+:\d{2}(:\d{2})?')
 	
 	
 	def handle_starttag(self,tag,attrs):
+		if tag in MetExtractor.EMPTY_ELEMS:
+			return
+		
 		attrDict = {i:j for i,j in attrs}
 		self._tagStack.put((tag,attrDict))
 		
@@ -43,6 +56,9 @@ class MetExtractor(HTMLParser):
 	
 	
 	def handle_endtag(self,tag):
+		if tag in MetExtractor.EMPTY_ELEMS:
+			return
+		
 		self._tagStack.get()
 		
 	
@@ -53,7 +69,7 @@ class MetExtractor(HTMLParser):
 		curtag,curattrs = self._tagStack.get()
 		self._tagStack.put((curtag,curattrs))
 		
-		if self._activeRegion and curtag != 'a':
+		if self._activeRegion and (curtag == 'p' or curtag == 'body'):
 			imgIDCandidate = data.strip()[:12]
 			if self.idRegex.fullmatch(imgIDCandidate) is not None:
 				sys.stdout.write('\n' + imgIDCandidate + ',')
